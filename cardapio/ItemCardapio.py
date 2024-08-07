a36658e09da8dd8f4106f58c8fbfff8e2a31b6a5
Vaalls <bielvalls4@gmail.com>
2024-08-07 20:48:22 -0300

    Criando o método aplicar_desconto na classe Item Cardapio para podermos aplicar desconto nos itens do cardapio.

--- cardapio/ItemCardapio.py
+++ cardapio/ItemCardapio.py
@@ -1,5 +1,15 @@
+from abc import abstractmethod
+
+
 class ItemCardapio():
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
 
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
+
+# Se torna um metodo obrigatorio para todas as classe que fazem herança
+
+Classe "App" a principal do nosso sistema para podermos rodar e testar o funcionamento do projeto.
