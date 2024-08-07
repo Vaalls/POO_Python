69900a147ef44974a3e80156d5c1fea656063a8d
Vaalls <bielvalls4@gmail.com>
2024-08-07 19:12:38 -0300

    Criando o método listar_resturante na classe Resturante para listar os restaurantes que estiver registrados. @classmethod -> Decorador que indica que o método é um método de classe, recebendo a própria classe como primeiro argumento.

--- Resturante.py
+++ Resturante.py
@@ -10,3 +10,11 @@ class Restaurante:
 
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
+
+    @classmethod
+    def listar_restaurantes(self):
+        print(f'{"Nome do restaurante".ljust(20)} | {"Categoria".ljust(20)} | '
+              f'{"Avaliação".ljust(20)} | {"Status"}')
+        for restaurant in Restaurante.restaurantes:
+            print(f'{restaurant._nome.ljust(20)} | {restaurant.categoria.ljust(20)} | '
+                  f'{str(restaurant.media_avaliacoes).ljust(20)} | {restaurant.ativo}')
