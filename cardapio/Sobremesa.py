a8a5a745bf447accbb8833274690e1b33391149b
Vaalls <bielvalls4@gmail.com>
2024-08-07 20:57:41 -0300

    Complementando a classe Sobremesa com os metodos que estavam faltando e tipificando as veriaveis.

--- cardapio/Sobremesa.py
+++ cardapio/Sobremesa.py
@@ -1,7 +1,13 @@
 from cardapio.ItemCardapio import ItemCardapio
 class Sobremesa(ItemCardapio):
-    def __init__(self, nome, preco, tipo, tamanho, descricao):
+    def __init__(self, nome:str, preco:float, tipo:str, tamanho:str, descricao:str):
         super().__init__(nome, preco)
         self._tipo = tipo
         self._tamanho = tamanho
         self._descricao = descricao
+
+    def __str__(self):
+        return f'Tipo da sobremesa: {self._tipo}'
+
+    def aplicar_desconto(self):
+        pass
