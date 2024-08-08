66e7e645ca8b040d682bfcbfeef0c04e8369e0b2
Vaalls <bielvalls4@gmail.com>
2024-08-07 21:02:18 -0300

    Complementando a classe Bebida com o metodo que estava faltando e tipificando as veriaveis.

--- cardapio/Bebida.py
+++ cardapio/Bebida.py
@@ -1,6 +1,9 @@
 from cardapio.ItemCardapio import ItemCardapio
 class Bebida(ItemCardapio):
-   def __init__(self, nome, preco, tamanho):
+   def __init__(self, nome:str, preco:float, tamanho:str):
        super().__init__(nome, preco)
        self._tamanho = tamanho
+
+   def aplicar_desconto(self):
+       self._preco -= (self._preco * 0.08)
 
