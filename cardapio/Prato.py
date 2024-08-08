03cb83443a83971106bf724e76729fa1aeec9e3b
Vaalls <bielvalls4@gmail.com>
2024-08-07 21:00:10 -0300

    Complementando a classe Prato com o metodo que estava faltando e tipificando as veriaveis.

--- cardapio/Prato.py
+++ cardapio/Prato.py
@@ -1,5 +1,8 @@
 from cardapio.ItemCardapio import ItemCardapio
 class Prato(ItemCardapio):
-    def __init__(self, nome, preco, descricao):
+    def __init__(self, nome:str, preco:float, descricao:str):
         super().__init__(nome, preco)
         self._descricao = descricao
+
+    def aplicar_desconto(self):
+        self.preco -= (self._preco * 0.05)
