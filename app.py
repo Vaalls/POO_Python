255a04232ecac3f7831637f74f3c98621b4a4426
Vaalls <bielvalls4@gmail.com>
2024-08-07 21:17:07 -0300

    Testando funcionalidades do sistema...

--- app.py
+++ app.py
@@ -13,12 +13,20 @@ restaurante.alternar_estado()
 print(f'O restaurante {restaurante.nome} {restaurante.ativo}, foi ativado com sucesso!')
 
 bebida_suco = Bebida('Coca-Cola', 9, 'Média')
-
 prato_macarrao = Prato('Macarrão', 30.85, 'Macarrão da casa')
 
 restaurante.adicionar_item(bebida_suco)
-
+restaurante.adicionar_item(prato_macarrao)
 restaurante.exibir_cardapio
+
+print()
+
+print('Cardapio após desconto\n')
+bebida_suco.aplicar_desconto()
+prato_macarrao.aplicar_desconto()
+restaurante.exibir_cardapio
+restaurante.receber_avaliacao(cliente,avaliacao._nota)
+print(restaurante.media_avaliacoes)
 
 
 
